b885846a03a56a473dcd094b06fd611bd7acf9e7
Arman19941113 <td19941113@gmail.com>
2021-01-26 16:40:17 +0800

    feat: add i18n

--- src/render/App.tsx
+++ src/render/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import t from '@/render/language';
 
-const Hello = () => {
-  return <div>Hello world</div>;
-};
+function Hello() {
+  return <div>{t('图片智能命名')}</div>;
+}
 
 export default function App() {
   return (
